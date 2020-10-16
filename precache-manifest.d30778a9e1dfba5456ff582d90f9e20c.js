03be849d13e385f9cc99c9f86ccfe5ad425c650a
Victor Bityukov <sylar167@gmail.com>
2020-10-16 19:54:05 +0700

    Updates

---
+++ precache-manifest.d30778a9e1dfba5456ff582d90f9e20c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d556bcc4dbd17ecef3e7a6c27804cba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bfc03d69e74bb3645a75",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "fe352d1ae9abea89fb76",
+    "url": "./static/css/main.f9ce8d49.chunk.css"
+  },
+  {
+    "revision": "bfc03d69e74bb3645a75",
+    "url": "./static/js/2.70f2bd5e.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.70f2bd5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe352d1ae9abea89fb76",
+    "url": "./static/js/main.f54620e5.chunk.js"
+  },
+  {
+    "revision": "2118d2cf221beb2ca1c5",
+    "url": "./static/js/runtime-main.d12f9e81.js"
+  },
+  {
+    "revision": "a348e6960c83001a28bc43d62d6f35dc",
+    "url": "./static/media/-.a348e696.mp3"
+  },
+  {
+    "revision": "8775de6de35bbefbc27ec105c14a29ee",
+    "url": "./static/media/—.8775de6d.mp3"
+  }
+]);
